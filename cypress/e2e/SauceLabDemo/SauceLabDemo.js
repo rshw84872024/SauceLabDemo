10725959b5d8bf20d3561790cdb1087d2a11e598
rshw84872024 <rahul.sonawane@matter.in>
2024-07-11 15:56:14 +0530

    new commit

--- cypress/e2e/SauceLabDemo/SauceLabDemo.js
+++ cypress/e2e/SauceLabDemo/SauceLabDemo.js
@@ -1,5 +1,7 @@
+// Importing necessary Cypress-Cucumber preprocessor steps
 import { Given, When, Then, And, Before, After } from 'cypress-cucumber-preprocessor/steps';
 
+// Before hook to run before any tests in this file or suite
 Before(() => {
     // Code to run before any tests in this file or suite
     // This code will run once before any tests in this file start
@@ -7,46 +9,51 @@ Before(() => {
     cy.reload();
 });
 
+// Step definitions using Cucumber syntax
+// Step: User should be on the Landing page of the website
 
 Given("User should on the Landing page of the wesite", () => {
+
     cy.visit("https://demo.nopcommerce.com/")
 });
-
+// Step: User clicks on Apparel Tab
 When("User clicks on Appereal Tab", () => {
     cy.get(".category-item").eq(1)
         .click();
 });
-
+// Step: User should see Apparel Tab open
 Then("User should see Apperal Tab open", () => {
     cy.get(".page-title")
         .contains("Apparel")
         .should("be.visible");
 
 });
+
+// Step: User clicks on Clothing
 When("User clicks on clicks on Clothing", () => {
     cy.get(".item-box").eq(1)
         .click();
 });
-
+// Step: User should see the clothing tab open
 Then("User should see the clothing tab open", () => {
     cy.get(".page-title")
         .contains("Clothing")
         .scrollIntoView()
         .should("be.visible")
 });
-
+// Step: User clicks on Add to cart on first item
 When("User clicks on Add to cart on first item", () => {
     cy.get(".buttons").eq(0)
         .click();
 
 });
-
+// Step: User should enter the text in text box
 Then("User should enter the text in text box", () => {
     cy.get(".textbox")
         .click()
         .type("ABC");
 });
-
+// Step: User clicks on 3 items add to cart on the page
 When("User clicks on 3 items add to cart on the page", () => {
     cy.get("#add-to-cart-button-29")
         .click()
@@ -58,47 +65,47 @@ When("User clicks on 3 items add to cart on the page", () => {
         .click();
 
 });
-
+// Step: User should scroll to Shopping cart
 Then("User should scroll to Shopping cart", () => {
     cy.get("#topcartlink")
         .scrollIntoView()
 });
-
+// Step: User clicks on Shopping cart
 When("User clicks on Shopping cart", () => {
     cy.get("#topcartlink")
         .click();
 });
-
+// Step: User Navigate to checkout
 Then("User Navigate to checkout", () => {
 cy.get(".cart-total")
 .scrollIntoView();
 });
-
+// Step: User clicks on checkout
 When("User clicks on checkout", () => {
 cy.get("#termsofservice")
 .click()
 .get("#checkout")
 .click();
 });
-
+// Step: User should Navigate to Billing page to Login
 Then("User should Navigate to Billing page to Login", () => {
 cy.get("#Email")
 .click()
-.type("Rahul_s1@gmail.com")
+.type("Rahul$@gmail.com")
 .get("#Password")
 .type("Rahul@123")
 .get(".button-1.login-button")
 .contains("Log in")
 .click();
 });
-
+// Step: User Fulfill all checkout Requirements
 When("User Fulfill all checkout Requirements", () => {
 cy.get("#termsofservice")
 .click()
 .get("#checkout")
 .click();
 });
-
+// Step: User should navigate to Confirm Page to confirm the shipment
 Then("User should naviagte to Confirm Page to confirm the shipment", () => {
 cy.get(".button-1").eq(5)
 .click()
@@ -112,7 +119,7 @@ cy.get(".button-1").eq(5)
 .click()
 
 });
-
+// Step: User should see confirmation page
 And("User should see confirmation page", () => {
 cy.get(".page-title")
 .contains("Thank you")
@@ -123,7 +130,7 @@ cy.get(".page-title")
 });
 
 
-
+// After hook to perform cleanup actions after all tests in this file or suite
 After(() => {
     // Code to clear data or perform cleanup actions
     // This code will run after all tests in this file have completed
